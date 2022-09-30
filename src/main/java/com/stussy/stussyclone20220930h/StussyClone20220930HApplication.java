5e32cc7de46cb720af6e4a06b34b2e1453be9917
1nyeong <inyeong0726@gmail.com>
2022-09-30 16:44:04 +0900

    stussy clone 1

---
+++ src/main/java/com/stussy/stussyclone20220930h/StussyClone20220930HApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220930HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220930HApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stussy/stussyclone20220930h/StussyClone20220930HApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220930HApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
