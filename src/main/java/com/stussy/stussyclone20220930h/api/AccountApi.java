604395571cf5443901779649028172ab1bf973b5
1nyeong <inyeong0726@gmail.com>
2022-10-04 17:48:08 +0900

    login,register 4

--- src/main/java/com/stussy/stussyclone20220930h/api/AccountApi.java
+++ src/main/java/com/stussy/stussyclone20220930h/api/AccountApi.java
@@ -3,6 +3,7 @@ package com.stussy.stussyclone20220930h.api;
 import com.stussy.stussyclone20220930h.dto.RegisteReqDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountApi {
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(RegisteReqDto registeReqDto){
+    public ResponseEntity<?> register(@RequestBody RegisteReqDto registeReqDto){
         System.out.println("회원가입 요청 데이터 : " + registeReqDto);
         return null;
     }
--- src/main/resources/static/js/account/register.js
+++ src/main/resources/static/js/account/register.js
@@ -1,7 +1,7 @@
 const registerButton = document.querySelector(".account-button");
 
 registerButton.onclick = () => {
-    const accountInput = document.querySelectorAll("account-input");
+    const accountInput = document.querySelectorAll(".account-input");
 
     let user = {
         lastName: accountInput[0].value,
@@ -9,20 +9,22 @@ registerButton.onclick = () => {
         email: accountInput[2].value,
         password: accountInput[3].value
     }
+
+    //JSON.stringfly() -> js 객체를 JSON문자열로 변환
+    //JSON.parse()     -> JSON문자열을 js 객체로 변환
     
-    let ajaxOption = {
-        async: false,                   //필수
-        type: "post",                   //필수
-        url: "/api/account/register",   //필수
-        data: user,                     //전송할 데이터가 있으면
-        dataType: "json",               //json 외 text 등을 사용할 수 있지만 js
-        success: (response) => {         //성공시에 실행될 메소드
+    $.ajax({
+        async: false,                       //필수
+        type: "post",                       //필수
+        url: "/api/account/register",       //필수
+        contentType: "application/json",    //전송 데이터가 json인 경우
+        data: JSON.stringify(user),         //전송할 데이터가 있으면
+        dataType: "json",                   //json 외 text 등을 사용할 수 있지만 js
+        success: (response) => {            //성공시에 실행될 메소드
             alert("회원가입 요청 성공");
         },
-        error: (error) => {             //실패시 실행될 메소드
+        error: (error) => {                 //실패시 실행될 메소드
             alert("회원가입 요청 실패");
         }
-    }
-
-    $.ajax(ajaxOption);
+    });
 }
