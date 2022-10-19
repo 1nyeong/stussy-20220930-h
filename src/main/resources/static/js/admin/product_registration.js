c33202cc7468b95d9667d9e07ae906611a923319
1nyeong <inyeong0726@gmail.com>
2022-10-19 15:47:20 +0900

    product

--- src/main/resources/static/js/admin/product_registration.js
+++ src/main/resources/static/js/admin/product_registration.js
@@ -1,69 +1,78 @@
-const RegisterEventService = {
-    getCategorySelectObj: () => document.querySelectorAll(".product-inputs")[0],
-    getNameInputObj: () => document.querySelectorAll(".product-inputs")[1],
-    getPriceInputObj: () => document.querySelectorAll(".product-inputs")[2],
-    getRegistInfo: () => document.querySelector(".regist-info"),
-    getRegistButtonObj: () => document.querySelector(".regist-button"),
-    getInfoTextareaObjs: () => document.querySelectorAll(".product-inputs"),
+class ProductMst{
+    #category;
+    #name;
+    #price;
+    #simpleInfo;
+    #detailInfo;
+    #optionInfo;
+    #managementInfo;
+    #shippingInfo;
 
-    init: function() {
-        this.getNameInputObj().disabled = true;
-        this.getPriceInputObj().disabled = true;
-        this.getRegistButtonObj().disabled = true;
-    },
+    constructor(category, name, price, simpleInfo, detailInfo, optionInfo, managementInfo, shippingInfo){
+        this.#category = category;
+        this.#name = name;
+        this.#price = price;
+        this.#simpleInfo = simpleInfo;
+        this.#detailInfo = detailInfo;
+        this.#optionInfo = optionInfo;
+        this.#managementInfo = managementInfo;
+        this.#shippingInfo = shippingInfo;
+    }
 
-    addCategorySelectEvent: function() {
-        this.getCategorySelectObj().onchange = () => {
-            if(this.getCategorySelectObj().value != "none"){
-                this.getNameInputObj().disabled = false;
-            }else{
-                this.getNameInputObj().disabled = true;
-            }
-            RegisterObj.category = this.getCategorySelectObj().value;
+    getCategory(){return this.#category;}
+    setCategory(){this.#category = category;}
+
+    getName(){return this.#name;}
+    setName(){this.#name = name;}
+    
+    getPrice(){return this.#price;}
+    setPrice(){this.#price = price;}
+    
+    getSimpleInfo(){return this.#simpleInfo;}
+    setSimpleInfo(){this.#simpleInfo = simpleInfo;}
+    
+    getDetailInfo(){return this.#detailInfo;}
+    setDetailInfo(){this.#detailInfo = detailInfo;}
+    
+    getOptionInfo(){return this.#optionInfo;}
+    setOptionInfo(){this.#optionInfo = optionInfo;}
+    
+    getManagementInfo(){return this.#managementInfo;}
+    setManagementInfo(){this.#managementInfo = managementInfo;}
+    
+    getShippingInfo(){return this.#shippingInfo;}
+    setShippingInfo(){this.#shippingInfo = shippingInfo;}
+
+    getObject(){
+        const obj = {
+            category: this.#category,
+            name: this.#name,
+            price: this.#price,
+            simpleInfo: this.#simpleInfo,
+            detailInfo: this.#detailInfo,
+            optionInfo: this.#optionInfo,
+            managementInfo: this.#managementInfo,
+            shippingInfo: this.#shippingInfo
         }
-    },
-
-    addNameInputEvent: function() {
-        this.getNameInputObj().onkeyup = () => {
-            if(this.getNameInputObj().value.length != 0){
-                this.getPriceInputObj().disabled = false;
-            }else{
-                this.getPriceInputObj().disabled = true;
-            }
-            RegisterObj.name = this.getNameInputObj().value;
-        }
-    },
-
-    addPriceInputEvent: function() {
-        this.getPriceInputObj().onkeyup = () => {
-            if(this.getPriceInputObj().value.length != 0){
-                this.getRegistButtonObj().disabled = false;
-                this.getRegistInfo().classList.remove("regist-info-invisible");
-            }else{
-                this.getRegistButtonObj().disabled = true;
-                this.getRegistInfo().classList.add("regist-info-invisible");
-            }
-            RegisterObj.price = this.getPriceInputObj().value;
-        }
-    },
-
-    addRegistButtonEvent: function() {
-        this.getRegistButtonObj().onclick = () => {
-            RegisterObj.simpleInfo = this.getInfoTextareaObjs()[3].value;
-            RegisterObj.detailInfo = this.getInfoTextareaObjs()[4].value;
-            RegisterObj.optionInfo = this.getInfoTextareaObjs()[5].value;
-            RegisterObj.managementInfo = this.getInfoTextareaObjs()[6].value;
-            RegisterObj.shippingInfo = this.getInfoTextareaObjs()[7].value;
-            
-            console.log(RegisterObj);
-            
-            RegisterEventService.createProductRequest();
-        }
+        return obj;
     }
 }
 
-const RegisterRequestService = {
-    createProductRequest: () => {
+
+//메인 로직
+class RegisterService{
+    constructor(){
+        new RegisterEventService();
+        this.loadRegister();
+    }
+
+    loadRegister(){
+
+    }
+}
+
+class RegisterApi{
+    createProductRequest(productMst){
         let responseResult = null;
 
         $.ajax({
@@ -85,27 +94,88 @@ const RegisterRequestService = {
     }
 }
 
-const RegisterObj = {
-    category: null,
-    name: null,
-    price: null,
-    simpleInfo: null,
-    detailInfo: null,
-    optionInfo: null,
-    managementInfo: null,
-    shippingInfo: null
-}
+class RegisterEventService {
+    #categorySelectObj;
+    #nameInputObj;
+    #priceInputObj;
+    #registButtonObj;
+    #infoTextareaObjs;
 
-const ProductRegistration = {
-    initRegisterEvent: () => {
-        RegisterEventService.init();
-        RegisterEventService.addCategorySelectEvent();
-        RegisterEventService.addNameInputEvent();
-        RegisterEventService.addPriceInputEvent();
-        RegisterEventService.addRegistButtonEvent();
+    constructor(){
+        this.#categorySelectObj = document.querySelectorAll(".product-inputs")[0];
+        this.#nameInputObj = document.querySelectorAll(".product-inputs")[1];
+        this.#priceInputObj = document.querySelectorAll(".product-inputs")[2];
+        this.#registButtonObj = document.querySelector(".regist-button");
+        this.#infoTextareaObjs = document.querySelectorAll(".product-inputs");
+
+        this.init();
+
+        this.addCategorySelectEvent();
+        this.addNameInputEvent();
+        this.addPriceInputEvent();
+        this.addRegistButtonEvent();
+    }
+
+    init(){
+        this.#nameInputObj().disabled = true;
+        this.#priceInputObj().disabled = true;
+        this.#registButtonObj().disabled = true;
+    }
+
+    addCategorySelectEvent() {
+        this.#categorySelectObj.onchange = () => {
+            if(this.#categorySelectObj.value != "none"){
+                this.#nameInputObj.disabled = false;
+            }else{
+                this.#nameInputObj.disabled = true;
+            }
+        }
+    }
+
+    addNameInputEvent() {
+        this.#nameInputObj.onkeyup = () => {
+            if(this.#nameInputObj.value.length != 0){
+                this.#priceInputObj.disabled = false;
+            }else{
+                this.#priceInputObj.disabled = true;
+            }
+        }
+    }
+
+    addPriceInputEvent() {
+        this.#priceInputObj.onkeyup = () => {
+            const registInfo = document.querySelector(".regist-info");
+
+            if(this.#priceInputObj.value.length != 0){
+                this.#registButtonObj.disabled = false;
+                registInfo.classList.remove("regist-info-invisible");
+
+            }else{
+                this.#registButtonObj.disabled = true;
+                registInfo.classList.add("regist-info-invisible");
+            }
+        }
+    }
+
+    addRegistButtonEvent() {
+        this.#registButtonObj.onclick = () => {
+            const category = this.#categorySelectObj.value;
+            const name = this.#nameInputObj.value;
+            const price = this.#priceInputObj.value;
+            const simpleInfo = this.#infoTextareaObjs()[3].value;
+            const detailInfo = this.#infoTextareaObjs()[4].value;
+            const optionInfo = this.#infoTextareaObjs()[5].value;
+            const managementInfo = this.#infoTextareaObjs()[6].value;
+            const shippingInfo = this.#infoTextareaObjs()[7].value;
+
+            const productMst = new  ProductMst(category, name, price, simpleInfo, detailInfo, optionInfo, managementInfo, shippingInfo);
+            
+            console.log(productMst.getObject);
+            
+        }
     }
 }
 
 window.onload = () => {
-    ProductRegistration.initRegisterEvent();
+    new RegisterService();
 }
